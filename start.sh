e2b5f16c5706855c58a7e89d26861664d65e4ce9
g-silvan <silvangehrig5@gmail.com>
2025-06-07 10:59:39 +0000

    upload

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-venv/bin/python main.py
+#!/bin/bash
+
+# Port aus der .env Datei auslesen
+WEB_PORT=$(grep -E '^WEB_PORT=' .env | cut -d '=' -f2)
+
+# Gunicorn starten
+venv/bin/gunicorn --bind 0.0.0.0:$WEB_PORT main:app
