a293272a2ffbe221086a6ea0a029f35c703f5569
g-silvan <silvangehrig5@gmail.com>
2025-06-07 11:38:51 +0000

    upload

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@
 WEB_PORT=$(grep -E '^WEB_PORT=' .env | cut -d '=' -f2)
 
 # Gunicorn starten
-venv/bin/gunicorn --bind 0.0.0.0:$WEB_PORT main:app
+venv/bin/gunicorn --workers 10 --timeout 120 --bind 0.0.0.0:$WEB_PORT main:app
